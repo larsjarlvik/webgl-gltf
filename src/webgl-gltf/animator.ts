ee4dfa6114ed2a08bf48f098031709790fa55473
ljarlvik <lars.jarlvik@agoda.com>
2020-04-10 12:00:40 +0700

    Clean up animation handling for future improvements

--- src/webgl-gltf/animator.ts
+++ src/webgl-gltf/animator.ts
@@ -1,5 +1,6 @@
-import { Model, KeyFrame, Skin, Channel } from './types/model';
+import { Model, KeyFrame, Skin, Transform } from './types/model';
 import { mat4, vec3, quat } from 'gl-matrix';
+import { ActiveAnimation } from './animation';
 
 const getPreviousAndNextKeyFrame = (keyFrames: KeyFrame[], animationTime: number) => {
     let next = keyFrames[0];
@@ -43,11 +44,11 @@ const getTransform = (keyFrames: KeyFrame[], duration: number) => {
     }
 };
 
-interface Transform {
+interface TransformMatrices {
     [key: number]: mat4;
 }
 
-const applyTransforms = (appliedTransforms: mat4[], model: Model, transforms: Transform, matrix: mat4, skin: Skin, nodeIndex: number) => {
+const applyTransforms = (appliedTransforms: mat4[], model: Model, transforms: TransformMatrices, matrix: mat4, skin: Skin, nodeIndex: number) => {
     const node = model.nodes[nodeIndex];
 
     if (transforms[node.id] !== undefined) {
@@ -69,47 +70,48 @@ const applyTransforms = (appliedTransforms: mat4[], model: Model, transforms: Tr
 };
 
 
+const get = (c: Transform, elapsed: number) => {
+    const t = c.translation.length > 0 ? getTransform(c.translation, elapsed) as vec3 : vec3.create();
+    const r = c.rotation.length > 0 ? getTransform(c.rotation, elapsed) as quat : quat.create();
+    const s = c.scale.length > 0 ? getTransform(c.scale, elapsed) as vec3 : vec3.fromValues(1, 1, 1);
+    return { t, r, s };
+}
+
+
 /**
  * Blends two animations and returns their transform matrices
  * @param model GLTF Model
- * @param current Current animation
- * @param currentElapsed Elapsed time for current animation
- * @param previous Previous animation
- * @param previouseElapsed Elapsed time for previous animation
+ * @param activeAnimations Currently running animations
  * @param blendTime How long the blend should be in milliseconds
  */
-const getAnimationTransforms = (model: Model, current?: Channel, currentElapsed?: number, previous?: Channel, previouseElapsed?: number, blendTime = 0) => {
-    if (!current || currentElapsed === undefined) {
-        return null;
-    }
-
+const getAnimationTransforms = (model: Model, activeAnimations: ActiveAnimation[], blendTime = 0) => {
     const transforms: { [key: number]: mat4 } = {};
 
-    Object.keys(current).forEach(c => {
-        const t1 = current[c].translation.length > 0 ? getTransform(current[c].translation, currentElapsed) as vec3 : vec3.create();
-        const r1 = current[c].rotation.length > 0 ? getTransform(current[c].rotation, currentElapsed) as quat : quat.create();
-        const s1 = current[c].scale.length > 0 ? getTransform(current[c].scale, currentElapsed) as vec3 : vec3.fromValues(1, 1, 1);
+    activeAnimations.forEach(rootAnimation => {
+        const blend = -((rootAnimation.elapsed - blendTime) / blendTime);
 
-        const blend = -((currentElapsed - blendTime) / blendTime);
-        if (previous && previouseElapsed !== undefined && blend > 0) {
-            const t2 = getTransform(previous[c].translation, previouseElapsed) as vec3;
-            const r2 = getTransform(previous[c].rotation, previouseElapsed) as quat;
-            const s2 = getTransform(previous[c].scale, previouseElapsed) as vec3;
+        Object.keys(model.animations[rootAnimation.key]).forEach(c => {
+            const transform = get(model.animations[rootAnimation.key][c], rootAnimation.elapsed);
 
-            if (t2 !== undefined) vec3.lerp(t1, t1, t2, blend);
-            if (r2 !== undefined) quat.slerp(r1, r1, r2, blend);
-            if (s2 !== undefined) vec3.lerp(s1, s1, s2, blend);
-        }
+            activeAnimations.forEach(ac => {
+                if (rootAnimation.key == ac.key || blend <= 0) return;
 
-        const localTransform = mat4.create();
-        const rotTransform = mat4.create();
-        mat4.fromQuat(rotTransform, r1 as quat);
+                const cTransform = get(model.animations[ac.key][c], ac.elapsed);
+                vec3.lerp(transform.t, transform.t, cTransform.t, blend);
+                quat.slerp(transform.r, transform.r, cTransform.r, blend);
+                vec3.lerp(transform.s, transform.s, cTransform.s, blend);
+            });
 
-        mat4.translate(localTransform, localTransform, t1 as vec3);
-        mat4.multiply(localTransform, localTransform, rotTransform);
-        mat4.scale(localTransform, localTransform, s1 as vec3);
+            const localTransform = mat4.create();
+            const rotTransform = mat4.create();
+            mat4.fromQuat(rotTransform, transform.r as quat);
 
-        transforms[c] = localTransform;
+            mat4.translate(localTransform, localTransform, transform.t as vec3);
+            mat4.multiply(localTransform, localTransform, rotTransform);
+            mat4.scale(localTransform, localTransform, transform.s as vec3);
+
+            transforms[c] = localTransform;
+        });
     });
 
     const appliedTransforms: mat4[] = [];
